cfbb8860afd23e4ac20ee8a5615db3c96e7a71fc
Shad <shadalam0024@gmail.com>
2025-08-07 15:26:55 +0530

    resend Otp function is created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -104,3 +104,47 @@ export const verifyAccount = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 200, res, "Email has been verified");
 });
+
+export const resendOtp = catchAsync(async (req, res, next) => {
+  const { email } = req.user;
+  if (!email) {
+    return next(new AppError("Email is required.", 400));
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(new AppError("User not found!", 404));
+  }
+
+  if (user.isVerified) {
+    return next(new AppError("THis account is already verified", 400));
+  }
+
+  const otp = generateOtp();
+  const otpExpires = Date.now() + 24 * 60 * 60 * 1000;
+  user.otp = otp;
+  user.otpExpires = otpExpires;
+
+  await user.save({ validateBeforeSave: false });
+
+  const htmlTemplate = loadTemplate("otpTemplate.hbs", {
+    title: "Otp Verification",
+    username: user.username,
+    otp,
+    message: "Your One Time Password (OTP) for account verification is : ",
+  });
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Resend otp for email verification",
+      html: htmlTemplate,
+    });
+
+    res.status(200).json({ status: "success", message: "New OTP is send to your email" });
+  } catch (error) {
+    (user.otp = undefined), (user.otpExpires = undefined), await user.save({ validateBeforeSave: false });
+    return next(new AppError("There is an error sending email, Pleasr try again late!r", 500));
+  }
+});
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { signup, verifyAccount } from "../controllers/authController.js";
+import { resendOtp, signup, verifyAccount } from "../controllers/authController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/verify", isAuthenticated, verifyAccount);
+userRouter.post("/resend-otp", isAuthenticated, resendOtp);
 
 export default userRouter;
