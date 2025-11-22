2b89c716d02684aaad60585afdec14dc8481ce70
Shad <shadalam0024@gmail.com>
2025-11-22 22:26:15 +0530

    Starting working on Setting page

---
+++ frontend/app/setting/page.tsx
@@ -0,0 +1,10 @@
+import Setting from '@/components/setting/Setting'
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <div><Setting/></div>
+  )
+}
+
+export default SettingsPage
--- frontend/components/Home/LeftMenu.tsx
+++ frontend/components/Home/LeftMenu.tsx
@@ -55,7 +55,7 @@ const LeftMenu = ({type}: {type: "home" | "profile"}) => {
           <Image src="/lists.png" alt="" height={24} width={24} className="w-5 h-5" />
           <span className="font-medium">Lists</span>
         </Link>
-        <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+        <Link href="/setting" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
           <Image src="/settings.png" alt="" height={24} width={24} className="w-5 h-5" />
           <span className="font-medium">Settings</span>
         </Link>
