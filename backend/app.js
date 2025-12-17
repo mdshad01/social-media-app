0195f4c948d4ce758b470c1accf235a9c8d56d43
Shad <shadalam0024@gmail.com>
2025-12-17 23:30:30 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://10.29.204.203:3000",
-      "https://social-media-app-6omb.vercel.app/api/v1",
+      "https://social-media-app-6omb.vercel.app",
       process.env.FRONTEND_URL // ✅ Use environment variable
     ].filter(Boolean),
     credentials: true,
